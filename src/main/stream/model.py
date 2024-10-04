bb62cdc9e2ea84318e6d99e8f7fac839a26cab70
Mkhgkk <emmachalz745@outlook.com>
2024-10-04 12:45:22 +0900

    feat: starting streaming based on schedule from database

--- src/main/stream/model.py
+++ src/main/stream/model.py
@@ -3,8 +3,48 @@
 # db = create_db_instance()
 from flask import current_app as app
 
+from main.shared import streams
+from main.shared import camera_controllers
+from streaming.video_streaming import VideoStreaming
+from utils.camera_controller import CameraController
+from main import tools
+
 class Stream:
     @staticmethod
     def get_all_streams():
         streams = app.db.streams.find()
         return streams
+    
+    @staticmethod
+    def start_stream(rtsp_link, model_name, stream_id, cam_ip=None, ptz_port=None, ptz_username=None, ptz_password=None, home_pan=None, home_tilt=None, home_zoom=None):
+        supports_ptz = all([cam_ip, ptz_port, ptz_username, ptz_password])
+        ptz_autotrack = all([home_pan, home_tilt, home_zoom])
+        
+        if stream_id in streams:
+            print("Stream is already running!")
+            return
+        
+        video_streaming = VideoStreaming(rtsp_link, model_name, stream_id, ptz_autotrack)
+        video_streaming.start_stream()
+        streams[stream_id] = video_streaming
+
+
+        # configure ptz
+        if supports_ptz:
+            cam_ip = cam_ip
+            ptz_port = ptz_port
+            ptz_username = ptz_username
+            ptz_password = ptz_password
+
+            camera_controller = CameraController(cam_ip, ptz_port, ptz_username, ptz_password)
+            camera_controllers[stream_id] = camera_controller
+
+    @staticmethod
+    def stop_stream(stream_id):
+        stream_id = stream_id
+
+        video_streaming = streams[stream_id]
+        video_streaming.stop_streaming()
+
+        del streams[stream_id]
+        del camera_controllers[stream_id]
--- src/run.py
+++ src/run.py
@@ -1,7 +1,56 @@
 from main import create_app
 import logging
+import time
+
+from appwrite.client import Client
+from appwrite.query import Query
+from appwrite.services.databases import Databases
+from appwrite.id import ID
+
+from main.stream.model import Stream
+
+client = Client()
+client.set_endpoint('http://192.168.0.10/v1')
+client.set_project('66f4c2e6001ef89c0f5c')
+client.set_key('standard_f6ffca3b91315e5263932317123368335466126c2bb58bf4201b64c9c3768ab3f364eabe0f7ba852fa24876c624393b306ce9a3f71db9c8fa951526a39e49721c80812d477196faad02d282ea51962a823c02f4580c1c95c67e6dba054653fa4d094a99ce4cb1a31ce52c57dbda6b925168446dd879de1e1d6321f0fa2f88cbd')
+
+databases = Databases(client)
+
+stream_docs = {}
+
+def fetch_streams():
+  streams = databases.list_documents(
+    database_id="isafe-guard-db",
+    collection_id="66f504260003d64837e5",
+  )
+
+  for stream in  streams['documents']:
+    stream_docs[stream["stream_id"]] = stream
+
+def fetch_schedules():
+  schedules = databases.list_documents(
+    database_id="isafe-guard-db",
+    collection_id="66fa20d600253c7d4503",
+    queries=[Query.greater_than('end_timestamp', int(time.time()))]
+  )
+
+  for schedule in schedules['documents']:
+    # active schedules
+    if schedule["start_timestamp"] < int(time.time()):
+      stream = stream_docs[schedule['stream_id']]
+      # print(schedule["stream_id"])
+      # print(stream_docs[schedule["stream_id"]])
+      Stream.start_stream(stream['rtsp_link'], schedule['model_name'], stream['stream_id'])
+      print(schedule)
+
+    
+
+
 
 if __name__ == "__main__":
+  print(int(time.time()))
+  fetch_streams()
+  fetch_schedules()
   app = create_app()
   app.run(host=app.config["FLASK_DOMAIN"], port=app.config["FLASK_PORT"])
 else:
