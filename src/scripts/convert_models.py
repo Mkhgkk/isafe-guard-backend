9e897a5440d897eaa83e92db147874abb8b1aabd
Mkhgkk <emmachalz745@outlook.com>
2024-12-04 15:33:07 +0900

    update: download model from url

--- src/scripts/convert_models.py
+++ src/scripts/convert_models.py
@@ -1,14 +1,32 @@
 import os
+import requests
 from ultralytics import YOLO
 
-model_files = ["PPEbest.pt"]
+model_urls = {
+    "PPEbest.pt": "https://storage.googleapis.com/isafe-models/PPEbest.pt"
+}
 
-# Loop through each model and export it
-for model_file in model_files:
+models_dir = "models"
+os.makedirs(models_dir, exist_ok=True)
+
+for model_name, url in model_urls.items():
+    model_path = os.path.join(models_dir, model_name)
+    if not os.path.exists(model_path):
+        print(f"Downloading {model_name} from {url}")
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(model_path, "wb") as f:
+                f.write(response.content)
+            print(f"{model_name} downloaded successfully.")
+        else:
+            print(f"Failed to download {model_name}. Status code: {response.status_code}")
+    else:
+        print(f"{model_name} already exists. Skipping download.")
+
+for model_file in model_urls.keys():
     print(f"Loading model: {model_file}")
     
-    # Load the YOLOv8 model
-    model = YOLO(os.path.join("models", model_file), task="detect")
+    model = YOLO(os.path.join(models_dir, model_file), task="detect")
 
     # Export the model to TensorRT format with specified parameters
     print(f"Exporting model: {model_file} to TensorRT format")
@@ -23,6 +41,6 @@ for model_file in model_files:
         # workspace=4,     # Workspace size in GB
         # device=1         # Specify the GPU device if necessary
     )
-
     print(f"Model {model_file} exported successfully.\n")
+
 
